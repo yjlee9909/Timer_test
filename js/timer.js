96871a5e21d19b3f9d762bb1696a177e0b765f59
yjlee9909 <yjlee9909@naver.com>
2022-11-08 11:48:43 +0900

    js추가

---
+++ js/timer.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('#app');
+const inpHour = main.querySelector('.bck-img-hour');
+const inpMin = main.querySelector('.bck-img-min');
+const inpSec = main.querySelector('.bck-img-sec');
+
+const disStartBtn = main.querySelector('.btn-start-disabled');
+const disResetBtn = main.querySelector('.btn-reset-disabled');
+
