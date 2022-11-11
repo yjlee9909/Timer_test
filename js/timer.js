64cb6217197803f69df5d7b0f46736a37291fc39
yjlee9909 <yjlee9909@naver.com>
2022-11-11 16:11:22 +0900

    시간 구현 전 기본 이벤트 추가

--- js/timer.js
+++ js/timer.js
@@ -10,13 +10,13 @@ const startImg = document.querySelector('.img-start');
 const resetImg = document.querySelector('.img-reset');
 
 const defStartBtn = document.querySelector('.defStart');
-console.log(defStartBtn)
 
 let timeHour = 00;
 let timeMin = 00;
 let timeSec = 00;
 
-function addTime(name) {
+
+const addTime = (name) => {
     // 초
     if (name === '.sec') {
         const secTxt = main.querySelector(name);
@@ -44,25 +44,60 @@ function addTime(name) {
         }
         
     }
-}
-function pauseBtn() {
+};
+const pauseBtn = () => {
     disStartBtn.addEventListener('click', () => {
-        // 왜 삭제가 안되는 거죠??....???
         disStartBtn.classList.remove('defStart');
+
+        // pause 버튼으로 변경해주기
+        disStartBtn.classList.add('pauseBtn');
+        startImg.setAttribute('src', './images/pause.png');
+
+        const pauseBtn = document.querySelector('.pauseBtn');
+        pauseBtn.addEventListener('click', () => {
+            disStartBtn.classList.remove('pauseBtn');
+            startImg.setAttribute('src', './images/start-default.png');
+            lightUpBtn();
+        })
+        
     });
 }
 
-function lightUpBtn() {
+const resetBtn = () => {
+    disResetBtn.addEventListener('click', () => {
+        disStartBtn.classList.remove('defStart');
+        startImg.setAttribute('src', './images/start-disabled.png');
+
+        disResetBtn.classList.remove('defReset');
+        resetImg.setAttribute('src', './images/reset-disabled.png');
+
+        timerReset();
+    })
+}
+const timerReset = () => {
+
+    timeHour = 00;
+    timeMin = 00;
+    timeSec = 00;
+    const secTxt = main.querySelector('.sec');
+    secTxt.textContent = String(timeSec).padStart(2,'0');
+
+    const minTxt = main.querySelector('.min');
+    minTxt.textContent = String(timeMin).padStart(2,'0');
+    const hourTxt = main.querySelector('.hour');
+    hourTxt.textContent = String(timeHour).padStart(2,'0');
+
+}
+
+const lightUpBtn = () => {
     disStartBtn.classList.add('defStart');
     startImg.setAttribute('src', './images/start-default.png');
-
-    
-    
 
     disResetBtn.classList.add('defReset');
     resetImg.setAttribute('src', './images/reset-default.png');
     // 불 들어온 start 버튼 누르면 pause 버튼으로 변경하기
     pauseBtn();
+    resetBtn();
 }
 
 
