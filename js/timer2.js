ca3ecc64d8d5eb6c9acc9c1287f7ca3ef0f7004c
yjlee9909 <yjlee9909@naver.com>
2022-11-18 20:50:48 +0900

    타이머 숫자길이 제한 추가

--- js/timer2.js
+++ js/timer2.js
@@ -4,9 +4,9 @@ const inpHour = main.querySelector('.hour');
 const inpMin = main.querySelector('.min');
 const inpSec = main.querySelector('.sec');
 
-const nonStartBtn = main.querySelector('.btn-start-disabled');
-const nonResetBtn = main.querySelector('.btn-reset-disabled');
-const onStartBtn = document.querySelector('.defStart');
+const nonStartBtn = main.querySelector('.btn-start.disabled');
+const nonResetBtn = main.querySelector('.btn-reset.disabled');
+// console.log(nonResetBtn)
 
 const startImg = document.querySelector('.img-start');
 const resetImg = document.querySelector('.img-reset');
@@ -23,3 +23,19 @@ inpMin.textContent = String(timeMin).padStart(2,'0');
 inpSec.textContent = String(timeSec).padStart(2,'0');
 
 // input 입력 글자수 제한하기 (number)
+function inputLenFunc() {
+    if (inpSec.value.length > 2) {
+        inpSec.value = inpSec.value.slice(0, 2);
+    };
+
+    if (inpMin.value.length > 2) {
+        inpMin.value = inpMin.value.slice(0, 2);
+    };
+    
+    if (inpHour.value.length > 2) {
+        inpHour.value = inpHour.value.slice(0, 2);
+    };
+}
+inpSec.addEventListener('input', inputLenFunc);
+inpMin.addEventListener('input', inputLenFunc);
+inpHour.addEventListener('input', inputLenFunc);
