ae695b03f2336966adb963363d41e252f14fc883
yjlee9909 <yjlee9909@naver.com>
2022-11-21 23:06:58 +0900

    add: 구현 완료

--- js/timer.js
+++ js/timer.js
@@ -1,168 +1,164 @@
+// 타이머 다시 구현
 const main = document.querySelector('#app');
 const inpHour = main.querySelector('.hour');
 const inpMin = main.querySelector('.min');
 const inpSec = main.querySelector('.sec');
 
-const disStartBtn = main.querySelector('.btn-start-disabled');
-const disResetBtn = main.querySelector('.btn-reset-disabled');
+const nonStartBtn = main.querySelector('.btn-start');
+const nonResetBtn = main.querySelector('.btn-reset');
+const pauseBtn = main.querySelector('.btn-pause.hidden')
+// console.log(nonResetBtn)
 
 const startImg = document.querySelector('.img-start');
 const resetImg = document.querySelector('.img-reset');
+const onStartBtn = document.querySelector('.startBtnOn');
+const inputGroup = [inpSec, inpMin, inpHour];
 
-const defStartBtn = document.querySelector('.defStart');
 
-
-
-let timeHour = 00;
-let timeMin = 00;
-let timeSec = 00;
-let time = 0;
 let timer;
-// if (time === 0) {
-//     disStartBtn.disabled = true;
-//     disResetBtn.disabled = true;
-// }
+let time = 0;
+let timeHour = 0;
+let timeMin = 0;
+let timeSec = 0;
 
-const addTime = (name) => {
-    // 초
-    if (name === '.sec') {
-        const secTxt = main.querySelector(name);
-        time += 10;
-        timeSec += 10;
-        if (timeSec > 60) {
-            timeSec = 60;
+// 00 할당
+inpHour.textContent = String(timeHour).padStart(2,'0');
+inpMin.textContent = String(timeMin).padStart(2,'0');
+inpSec.textContent = String(timeSec).padStart(2,'0');
+
+// 초기 start 버튼 비활성화하기
+nonStartBtn.setAttribute('disabled', true);
+nonResetBtn.setAttribute('disabled', true);
+
+// input 입력 글자수 제한하기 (number)
+const inputLenFunc = () => {
+    if (inpSec.value.length > 2) {
+        inpSec.value = inpSec.value.slice(0, 2);
+    };
+
+    if (inpMin.value.length > 2) {
+        inpMin.value = inpMin.value.slice(0, 2);
+    };
+
+    if (inpHour.value.length > 2) {
+        inpHour.value = inpHour.value.slice(0, 2);
+    };
+    // console.log(inpSec.value)
+}
+
+const activeBtn = () => {
+    switch(!(inpSec.value || inpMin.value || inpHour.value)) {
+        case false:
+            startImg.src = './images/start-default.png';
+            resetImg.src = './images/reset-default.png';
+            break;
+    } 
+    // 값 입력하고 start 버튼 클릭한 경우 버튼 활성화
+    nonStartBtn.removeAttribute('disabled');
+    nonResetBtn.removeAttribute('disabled');
+}
+const changeBtn = () => {
+    nonStartBtn.classList.add('hidden');
+    pauseBtn.classList.remove('hidden');
+    
+}
+const pauseTime = () => {
+    nonStartBtn.classList.remove('hidden');
+    pauseBtn.classList.add('hidden');
+    clearInterval(timer);
+}
+
+const startTime = (totalTime) => {
+    // console.log(totalTime)
+    timer = setInterval(() => {
+        totalTime--;
+        updateTotalTime(totalTime);
+        // console.log(totalTime)
+        if (totalTime <= 0) {
+            clearInterval(timer);
+            alert('시간이 종료되었습니다!! ⏰');
+            inpHour.value = String(timeHour).padStart(2,'0');
+            inpMin.value = String(timeMin).padStart(2,'0');
+            inpSec.value = String(timeSec).padStart(2,'0');
+
+            nonStartBtn.classList.remove('hidden');
+            pauseBtn.classList.add('hidden');
+            startImg.src = './images/start-disabled.png';
+            resetImg.src = './images/reset-disabled.png';
         }
-        timeSec = String(time % 60).padStart(2,'0');
-        secTxt.textContent = timeSec;
-        // console.log(time)
+    }, 1000);
+}
+const updateTotalTime = (totalTime) => {
+    const secUpd = totalTime % 60;
+    const minUpd = Math.floor((totalTime / 60) % 60);
+    const hourUpd = Math.floor((totalTime / 60) / 60);
+    // console.log(minUpd, secUpd)
+    // console.log(minUpd)
+    // console.log(hourUpd)
+
+    // 초
+    if (secUpd < 10) {
+        inpSec.value = secUpd.toString().padStart(2, "0")
+    } else {
+        inpSec.value = secUpd;
     }
     // 분
-    if (name === '.min') {
-        const minTxt = main.querySelector(name);
-        timeMin += 1;
-        if (timeMin < 10) {
-            minTxt.textContent = String(timeMin).padStart(2,'0');
-        } else {
-            if (timeMin > 60) {
-                timeMin = 60;
-            } else {
-                time += 60;
-            }
-            minTxt.textContent = timeMin;
-        }
+    if (minUpd < 10) {
+        inpMin.value = minUpd.toString().padStart(2, "0")
+    } else {
+        inpMin.value = minUpd;
     }
-    // 시
-    if (name === '.hour') {
-        const hourTxt = main.querySelector(name);
-        timeHour += 1;
-        if (timeHour < 10) {
-            hourTxt.textContent = String(timeHour).padStart(2,'0');
-        } else {
-            if (timeHour > 24) {
-                timeHour = 24;
-            } else {
-                time += 3600;
-                // timeHour = Math.floor(time / 3600);
-            }
-            hourTxt.textContent = timeHour;
-        }
-        
+    // 시간
+    if (hourUpd < 10) {
+        inpHour.value = hourUpd.toString().padStart(2, "0")
+    } else {
+        inpHour.value = hourUpd;
     }
-};
-const pauseBtn = () => {
-    disStartBtn.addEventListener('click', () => {
-        disStartBtn.classList.remove('defStart');
-
-        // pause 버튼으로 변경해주기
-        disStartBtn.classList.add('pauseBtn');
-        startImg.setAttribute('src', './images/pause.png');
-
-        const pauseBtn = document.querySelector('.pauseBtn');
-        pauseBtn.addEventListener('click', () => {
-            disStartBtn.classList.remove('pauseBtn');
-            startImg.setAttribute('src', './images/start-default.png');
-            lightUpBtn();
-        })
-        
-    });
-}
-
-const resetBtn = () => {
-    disResetBtn.addEventListener('click', () => {
-        disStartBtn.classList.remove('defStart');
-        startImg.setAttribute('src', './images/start-disabled.png');
-
-        disResetBtn.classList.remove('defReset');
-        resetImg.setAttribute('src', './images/reset-disabled.png');
-
-        timerReset();
-    })
-}
-const timerReset = () => {
-
-    timeHour = 00;
-    timeMin = 00;
-    timeSec = 00;
-    const secTxt = main.querySelector('.sec');
-    secTxt.textContent = String(timeSec).padStart(2,'0');
-
-    const minTxt = main.querySelector('.min');
-    minTxt.textContent = String(timeMin).padStart(2,'0');
-    const hourTxt = main.querySelector('.hour');
-    hourTxt.textContent = String(timeHour).padStart(2,'0');
-
-}
-
-const lightUpBtn = () => {
-    disStartBtn.classList.add('defStart');
-    startImg.setAttribute('src', './images/start-default.png');
-
-    disResetBtn.classList.add('defReset');
-    resetImg.setAttribute('src', './images/reset-default.png');
-    // 불 들어온 start 버튼 누르면 pause 버튼으로 변경하기
-    pauseBtn();
-    resetBtn();
-    timerStart();
-}
-
-// 타이머 기능 
-// 시간 값이 > 0 이라면 1초씩 줄어들기
-// 시간, 분, 초까지 다 합산하여 1초씩 줄어들기
-// const timerStart = () => {
-//     timer = setInterval(() => {
-//         time--;
-//         timeSec--;
-//         // 10초 미만
-//         if (timeSec < 10) {
-//             inpSec.textContent = String(timeSec).padStart(2, '0');
-//         } else {
-//             inpSec.textContent = timeSec;
-//         }
-
-//         // 분까지
-//         // if (timeMin > 0 && timeMin < )
-//         if (timeMin < 10) {
-//             inpMin.textContent = String(timeMin).padStart(2, '0');
-//         } else {
-//             inpMin.textContent = timeMin;
-//         }
-//         timeMin--;
-
-//     }, 1000);
-// }
-
-
-inpSec.addEventListener('click', () => {
-    addTime('.sec');
-    lightUpBtn();
     
-});
-inpMin.addEventListener('click', () => {
-    addTime('.min');
-    lightUpBtn();
-});
-inpHour.addEventListener('click', () => {
-    addTime('.hour');
-    lightUpBtn();
-});
+}
 
+const resetTime = () => {
+    clearInterval(timer);
+    inpHour.value = String(timeHour).padStart(2,'0');
+    inpMin.value = String(timeMin).padStart(2,'0');
+    inpSec.value = String(timeSec).padStart(2,'0');
+
+    nonStartBtn.setAttribute('disabled', true);
+    nonResetBtn.setAttribute('disabled', true);
+    nonStartBtn.classList.remove('hidden');
+    pauseBtn.classList.add('hidden');
+    startImg.src = './images/start-disabled.png';
+    resetImg.src = './images/reset-disabled.png';
+}
+
+
+inputGroup.map(item => item.addEventListener('input', inputLenFunc));
+
+// 값 입력받으면 버튼 활성화
+inputGroup.map(item => item.addEventListener('keyup', activeBtn));
+
+// 값 초기화
+inputGroup.map(item => item.addEventListener('click', (e) => {
+    e.currentTarget.value = '';
+}));
+
+nonStartBtn.addEventListener('click', () => {
+    // 값이 빈 경우 0으로 채워주기
+    if (inpHour.value === '') {
+        inpHour.value = String(timeHour).padStart(2,'0');
+    }
+    if (inpMin.value === '') {
+        inpMin.value = String(timeMin).padStart(2,'0');
+    }
+    if (inpSec.value === '') {
+        inpSec.value = String(timeSec).padStart(2,'0');
+    }
+    const secNum = parseInt(inpSec.value);
+    const minNum = parseInt(inpMin.value);
+    const houNum = parseInt(inpHour.value);
+    const totalTime = secNum + minNum * 60 + houNum * 60 * 60;
+    startTime(totalTime);
+    changeBtn();
+});
+pauseBtn.addEventListener('click', pauseTime);
+nonResetBtn.addEventListener('click', resetTime);
